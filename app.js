650804e3c304bd6a3655530b25a834e78d09821e
Robert McKenney <robert@mckenney.ca>
2021-04-12 12:26:47 -0400

    Complete lab demo

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import morgan from 'morgan'
+import logger from './startup/logger.js'
 import express from 'express'
+import compression from 'compression'
+import helmet from 'helmet'
 import cors from 'cors'
 import sanitizeMongo from 'express-mongo-sanitize'
 import handleErrors from './middleware/handleErrors.js'
@@ -11,10 +14,23 @@ import authRouter from './routes/auth/index.js'
 import connectDatabase from './startup/connectDatabase.js'
 connectDatabase()
 
+const log = logger.child({ module: 'expressApp' })
 const app = express()
 
+log.info(`The running environment is: ${process.env.NODE_ENV}`)
+log.warn(`The running environment is: ${app.get('env')}`) // if NODE_ENV is undefined, returns 'development'
+
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+)
+app.use(helmet())
+app.use(compression())
 app.use(morgan('tiny'))
-app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(sanitizeMongo())
--- startup/connectDatabase.js
+++ startup/connectDatabase.js
@@ -1,20 +1,26 @@
 import mongoose from 'mongoose'
-import createDebug from 'debug'
-const debug = createDebug('maddemo:db')
+import config from 'config'
+import logger from './logger.js'
+
+const log = logger.child({ module: 'connectDB' })
+const dbConf = config.get('db')
+const jwt = config.get('jwt')
+
+log.info(`jwt key: ${jwt.secretKey}`)
 
 export default function () {
   mongoose
-    .connect('mongodb://localhost:27017/mad9124', {
+    .connect(`mongodb://${dbConf.host}:${dbConf.port}/${dbConf.dbName}`, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
     })
     .then(() => {
-      debug('Successfully connected to MongoDB ...')
+      log.info('Successfully connected to MongoDB ...')
     })
     .catch((err) => {
-      debug('Error connecting to MongoDB ... ', err.message)
+      log.error('Error connecting to MongoDB ... ', err.message)
       process.exit(1)
     })
 }
